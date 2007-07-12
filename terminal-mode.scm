8a744abf1f4383146d0a4eec2064d57cc905194e
Duncan Mak <duncan@a-chinaman.com>
2007-07-12 15:53:23 -0400

    Fix up terminal-input-mode. Begin work on terminal-output-mode, it's incomplete right now.

--- terminal-mode.scm
+++ terminal-mode.scm
@@ -12,9 +12,7 @@
     (let* ((info      (tty-info port))
            (canonical (bitwise-and (tty-info:local-flags info)
                                    ttyl/canonical)))
-      (if (zero? canonical)
-          'raw
-          'cooked))))
+      (if (zero? canonical) 'raw 'cooked))))
 
 (define (set-input-terminal-mode mode . args)
   (let-optionals args ((port (current-input-port)))
@@ -28,7 +26,7 @@
     (let ((orig (tty-info port)))
      (set-input-terminal-mode mode port)
      (thunk)
-     (set-tty-info/flush port orig))))
+     (set-tty-info/now port orig))))
 
 (define-syntax with-input-terminal-mode
   (syntax-rules ()
@@ -42,40 +40,89 @@
      (call-with-input-terminal-mode mode
          (lambda () body ...)))))
 
-(define $input-terminal-cooked-mode (make-fluid (tty-info (current-input-port))))
-
-(define (input-terminal-cooked-mode)
-  (fluid $input-terminal-cooked-mode))
+(define input-terminal-cooked-mode (tty-info (current-input-port)))
 
 (define (input-terminal-raw-mode)
-  (let* ((info   (input-terminal-cooked-mode))
+  (let* ((info   input-terminal-cooked-mode)
          (ispeed (tty-info:input-speed info))
          (ospeed (tty-info:output-speed info))
          (min    1)
          (time   0))
     (make-tty-info
      ;; c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
-     (bitwise-not
-      (bitwise-and ttyin/ignore-break
-                   ttyin/interrupt-on-break
-                   ttyin/mark-parity-errors
-                   ttyin/7bits ttyin/nl->cr
-                   ttyin/ignore-cr ttyin/cr->nl
-                   ttyin/output-flow-ctl))
-     ;; c_oflag &= ~OPOST;
-     (bitwise-not ttyout/enable)
+     (bitwise-and
+      (tty-info:input-flags info)
+      (bitwise-not
+       (bitwise-ior ttyin/ignore-break
+                    ttyin/interrupt-on-break
+                    ttyin/mark-parity-errors
+                    ttyin/7bits ttyin/nl->cr
+                    ttyin/ignore-cr ttyin/cr->nl
+                    ttyin/output-flow-ctl)))
+
+     (tty-info:output-flags info)
 
      ;; c_cflag &= ~(CSIZE|PARENB);
-     (bitwise-not
-      (bitwise-and ttyc/char-size
-                   ttyc/enable-parity
-                   ttyc/char-size8))
+     (bitwise-and
+      (tty-info:control-flags info)
+      (bitwise-not
+       (bitwise-ior ttyc/char-size
+                    ttyc/enable-parity)))
 
      ;; c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
-     (bitwise-not
-      (bitwise-and ttyl/echo ttyl/echo-nl
-                   ttyl/canonical
-                   ttyl/enable-signals
-                   ttyl/extended))
+     (bitwise-and
+      (tty-info:local-flags info)
+      (bitwise-not
+       (bitwise-ior ttyl/echo ttyl/echo-nl
+                    ttyl/canonical
+                    ttyl/enable-signals
+                    ttyl/extended)))
 
      ispeed ospeed min time)))
+
+;; (define (output-terminal-mode . args)
+;;   (let-optionals args ((port (current-output-port)))
+;;     (let* ((info      (tty-info port))
+;;            (canonical (bitwise-and (tty-info:output-flags info)
+;;                                    ttyout/enable)))
+;;       (if (zero? canonical) 'raw 'cooked))))
+
+;; (define (set-output-terminal-mode mode . args)
+;;   (let-optionals args ((port (current-output-port)))
+;;     (case mode
+;;       ((cooked) (set-tty-info/now port (output-terminal-cooked-mode)))
+;;       ((raw)    (set-tty-info/now port (output-terminal-raw-mode)))
+;;       (else     (error "This is not a valid terminal mode: " mode)))))
+
+;; (define (call-with-output-terminal-mode mode thunk . args)
+;;   (let-optionals args ((port (current-output-port)))
+;;     (let ((orig (tty-info port)))
+;;       (set-output-terminal-mode mode port)
+;;       (thunk)
+;;       (set-tty-info/flush port orig))))
+
+;; (define-syntax with-output-terminal-mode
+;;   (syntax-rules ()
+;;     ((_ port mode body...)
+;;      (call-with-output-terminal-mode mode
+;;          (lambda () body ...) port))))
+
+;; (define-syntax with-current-output-terminal-mode
+;;   (syntax-rules ()
+;;     ((_ mode body ...)
+;;      (call-with-output-terminal-mode mode
+;;          (lambda () body ...)))))
+
+;; (define $output-terminal-cooked-mode (make-fluid (tty-info (current-output-port))))
+
+;; (define (output-terminal-cooked-mode)
+;;   (fluid $output-terminal-cooked-mode))
+
+;; (define (output-terminal-raw-mode)
+;;   (let* ((info   (output-terminal-cooked-mode))
+;;          (ispeed (tty-info:input-speed info))
+;;          (ospeed (tty-info:output-speed info))
+;;          (min    1)
+;;          (time   0))
+;;     (make-tty-info
+;;      )))
