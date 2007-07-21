d6492d1e381f1be5718bccc757524ed3a591e246
Duncan Mak <duncan@a-chinaman.com>
2007-07-21 05:19:13 -0400

    Fix keychord->string to handle special-keychords (RET, <backspace> and SPC)

--- keychord.scm
+++ keychord.scm
@@ -1,3 +1,5 @@
+;;; -*- Mode: Scheme; scheme48-package: linedit -*-
+
 (define-record-type keychord
   (%make-keychord key control? meta?)
   keychord?
@@ -6,13 +8,11 @@
   (meta?    keychord:meta-down?))
 
 (define (make-keychord key . modifiers)
-  (let-optionals modifiers ((control-down  #f)
+  (let-optionals modifiers ((control-down  (char-iso-control? key))
                             (meta-down     #f))
-    (if (not (char? key))
-        (error "This is not a valid keychord:" key)
-        (%make-keychord key control-down meta-down))))
+    (%make-keychord (key->char key) control-down meta-down)))
 
-(define (char->key c)
+(define (key->char c)
   (if (char-iso-control? c)
       (ascii->char (+ 96 (char->ascii c)))
       c))
@@ -21,16 +21,10 @@
   (or meta
       (not (zero? (bitwise-and #x80 n)))))
 
-(define (keychord c . args)
-  (let-optionals args ((meta #f))
-    (make-keychord (char->key c)
-                   (char-iso-control? c)
-                   (meta-pressed? (char->ascii c) meta))))
-
 (define (keychord=? chord1 chord2)
   (if (not (and (keychord? chord1)
                 (keychord? chord2)))
-      (error "Invalid input")
+      (error "Not chords")
       (and (char=? (keychord:key chord1)
                    (keychord:key chord2))
            (eq?    (keychord:control-down? chord1)
@@ -38,15 +32,38 @@
            (eq?    (keychord:meta-down? chord1)
                    (keychord:meta-down? chord2)))))
 
-(define (keychord->string chord)
-  (let ((output (open-output-string)))
-    (if (keychord:control-down? chord)
-        (display "C-" output))
-    (if (keychord:meta-down? chord)
-        (display "M-" output))
-    (display (keychord:key chord) output)
-    (get-output-string output)))
+(define (keychord-hash chord)
+  (* (char->ascii (keychord:key chord))
+     (if keychord:control-down? 1 255)
+     (if keychord:meta-down? 1 255)))
 
+(define (special-keychord name)
+  (case name
+    ((lf)     (make-keychord (ascii->char 10)))
+    ((cr)     (make-keychord (ascii->char 13)))
+    ((space)  (make-keychord (ascii->char 32)))
+    ((bksp)   (make-keychord (ascii->char 223) #t))))
+
+(define (special-keychord->string chord)
+  (cond
+   ((or (keychord=? chord (special-keychord 'lf))
+        (keychord=? chord (special-keychord 'cr)))
+    "RET")
+   ((keychord=? chord (special-keychord 'bksp)) "<backspace>")
+   ((keychord=? chord (special-keychord 'space)) "SPC")
+   (else #f)))
+
+(define (keychord->string chord)
+  (let ((output (open-output-string))
+        (key    (keychord:key chord)))
+     (or (special-keychord->string chord)
+         (begin
+           (if (keychord:control-down? chord)
+               (display "C-" output))
+           (if (keychord:meta-down? chord)
+               (display "M-" output))
+           (format output "~A (~A)" key (char->ascii key))
+           (get-output-string output)))))
 
 (define (string->keychord s)
   (let* ((len  (string-length s))
@@ -60,5 +77,5 @@
   (let-optionals args ((port (current-input-port)))
     (let ((c (read-char port)))
       (if (char=? (ascii->char 27) c)
-          (keychord (read-char) #t)
-          (keychord c)))))
+          (make-keychord (read-char) (char-iso-control? c) #t)
+          (make-keychord c)))))
