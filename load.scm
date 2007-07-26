0bc66b62e7b1bc6ca7c919d5bf4a09b4d547ccc1
Duncan Mak <duncan@a-chinaman.com>
2007-07-26 04:21:14 -0400

    Fix load order.

--- load.scm
+++ load.scm
@@ -1,8 +1,11 @@
 ;;; -*- Mode: Scheme; scheme48-package: (exec) -*-
+;;;
+;;; ,config ,load terminfo/packages.scm linedit/interfaces.scm linedit/packages.scm
+;;;
 (user)
 (config '(load "terminfo/packages.scm"
                "linedit/interfaces.scm"
                "linedit/packages.scm"))
 
-(open 'linedit)
 (open 'terminfo)
+(open 'linedit)
