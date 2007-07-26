839a1e75f1f0424a99bedc800144c0e2a0de62b8
Duncan Mak <duncan@a-chinaman.com>
2007-07-26 04:24:01 -0400

    Print only the integer value, instead of the char.

--- tests/showkeys.scm
+++ tests/showkeys.scm
@@ -1,7 +1,7 @@
 (with-current-input-terminal-mode 'raw
   (let loop ()
     (let* ((c  (read-char)))
-      (format #t "You typed *~A (~A)*~%" c (char->ascii c))
-      (if (char=? c #\q) 
+      (format #t "You typed '~A'~%" (char->ascii c))
+      (if (char=? c #\q)
           (display "bye bye now\n")
           (loop)))))
