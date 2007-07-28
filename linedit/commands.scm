9d6bfbe61348e9dfce03385b86357b9eda78da09
Duncan Mak <duncan@a-chinaman.com>
2007-07-28 04:20:22 -0400

    If the command does not make use of the key-character argument, then it is optional.

--- linedit/commands.scm
+++ linedit/commands.scm
@@ -3,7 +3,7 @@
 (define (lookup-meta-keymap l k)
   (process k l meta-keymap))
 
-(define (show-newline l k)
+(define (show-newline l . k)
   (newline) (display cr) l)
 
 (define (insert-char l k)
@@ -12,39 +12,39 @@
   (tputs (exit-insert-mode))
   (line-insert l k))
 
-(define (delete-backward-char l k)
+(define (delete-backward-char l . k)
   (if (null? (line:left l))
       l
       (begin (tputs (cursor-left))
              (tputs (delete-character))
              (shift-left l))))
 
-(define (delete-char l k)
+(define (delete-char l . k)
   (if (null? (line:right l))
       l
       (begin (tputs (delete-character))
              (shift-right l))))
 
-(define (move-beginning-of-line l k)
+(define (move-beginning-of-line l . k)
   (tputs (column-address 0))
   (beginning-of-line l))
 
-(define (move-end-of-line l k)
+(define (move-end-of-line l . k)
   (tputs (column-address (+ 1 (line-length l))))
   (end-of-line l))
 
-(define (backward-char l k)
+(define (backward-char l . k)
   (if (null? (line:left l))
       l
       (begin (tputs (cursor-left))
              (shift-left l (prev-char l)))))
 
-(define (forward-char l k)
+(define (forward-char l . k)
   (if (null? (line:right l))
       l
       (begin (tputs (cursor-right))
              (shift-right l (next-char l)))))
 
-(define (kill-line l k)
+(define (kill-line l . k)
   (tputs (clr-eol))
   (make-line (line:left l) '()))
