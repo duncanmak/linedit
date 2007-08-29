4d96ec3eed6cab6165ee7dc605c8e7de3f306140
Duncan Mak <duncan@a-chinaman.com>
2007-08-29 03:23:31 -0400

    Add a friendly helper for exploring the intricacies of various terminals

--- tests/showkeys.scm
+++ tests/showkeys.scm
@@ -1,5 +1,5 @@
-(define-structure showkeys (export show-keys)
-  (open scheme ascii formats terminal-mode)
+(define-structure showkeys (export show-keys show-keyseq)
+  (open scheme ascii formats srfi-13 terminal-mode)
   (begin
     (define (show-keys)
      (with-current-input-terminal-mode 'raw
@@ -8,4 +8,7 @@
            (if (char=? c #\q)
                (begin (newline) (display "bye bye now") (newline))
                (begin (format #t "~A " (char->ascii c))
-                      (loop)))))))))
+                      (loop)))))))
+    (define (show-keyseq s)
+      (string-for-each (lambda (c) (format #t "~a " (char->ascii c))) s) )
+    ))
